b6bac5a22429c43b290f563df9ef6d6e7a7c19be
jcrowe91 <jcrowe9119@gmail.com>
2022-03-02 21:10:01 -0600

    Added UpdateContact method

--- PhoneBookApplication/Program.cs
+++ PhoneBookApplication/Program.cs
@@ -8,7 +8,8 @@ namespace PhoneBookConsoleUI
     {
         static void Main(string[] args)
         {
-            string userAnswer;            
+            string userAnswer;
+            bool cont = true;
             var contacts = new List<Contact>();
             #region TEST CONTACT
             var test = new Contact();
@@ -23,25 +24,30 @@ namespace PhoneBookConsoleUI
 
             Console.WriteLine("Welcome to your phonebook application!");
 
+            while (cont == true)
+            {
 
-            Console.WriteLine($"What would you like to do?\n1: View All Contacts\n2: Create a New Contact\n3: Update A Contact\n4: Delete A Contact\n5: Exit");
-            userAnswer = Convert.ToString(Console.ReadLine());          
-            
+
+                Console.WriteLine($"What would you like to do?\n1: View All Contacts\n2: Create a New Contact\n3: Update A Contact\n4: Delete A Contact\n5: Exit");
+                userAnswer = Console.ReadLine();
+
                 if (userAnswer == "1") //View All
                 {
                     SeeAllContacts(contacts);
-                  
+
 
                 }
                 else if (userAnswer == "2") //Create
                 {
+                    Console.Clear();
+                    Console.WriteLine("Create New Contact:");
                     var newContact = CreateContact();
                     contacts.Add(newContact);
 
                 }
                 else if (userAnswer == "3") //Update
                 {
-                    Console.WriteLine("Three");
+                    UpdateContact(contacts);
                 }
                 else if (userAnswer == "4") //Delete
                 {
@@ -55,28 +61,42 @@ namespace PhoneBookConsoleUI
                 else
                 {
                     Console.WriteLine("Please enter a valid response");
-                    
+
                 }
-            
+            }
+        }
+
+        private static void UpdateContact(List<Contact> contacts)
+        {
+            for (int i = 0; i < contacts.Count; i++)
+            {
+                Console.WriteLine($"ID: {i + 1} {contacts[i].FirstName} {contacts[i].LastName}\nPhone Number: {contacts[i].PhoneNumber}\nEmail Address: {contacts[i].EmailAddress}\nHome Address: {contacts[i].HomeAddress}");
+                Console.WriteLine("-----------------------------");
+            }
+
+            Console.WriteLine("What is the ID of the contact you'd like to update?");
+            var contactID = Convert.ToInt32(Console.ReadLine());
+
+            contacts[contactID - 1] = CreateContact();
+            Console.WriteLine("Success!");
         }
 
         public static void SeeAllContacts(List<Contact> contacts)
         {
+            Console.Clear();
             foreach (var item in contacts)
-            {
-                Console.Clear();
+            {                              
                 Console.WriteLine($"{item.FirstName} {item.LastName}\nPhone Number: {item.PhoneNumber}\nEmail Address: {item.EmailAddress}\nHome Address: {item.HomeAddress}");
+                Console.WriteLine("-----------------------------");
             }
         }
 
         public static Contact CreateContact()
         {
-            var contact = new Contact();
-            Console.Clear();
-            Console.WriteLine("Create New Contact:");
-            Console.WriteLine("What is the first name of the person you want to add?");            
+            var contact = new Contact();                        
+            Console.WriteLine("What is the first name of the person?");            
             contact.FirstName = Console.ReadLine();
-            Console.WriteLine("What is the last name of the person you want to add?");
+            Console.WriteLine("What is the last name of the person?");
             contact.LastName = Console.ReadLine();
             Console.WriteLine("What is the phone number of the person?");
             contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
@@ -86,6 +106,8 @@ namespace PhoneBookConsoleUI
             contact.HomeAddress = Console.ReadLine();
             return contact;
         }
+
+        
 
     }   
 }
