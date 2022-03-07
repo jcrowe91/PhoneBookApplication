b8b212660f11f18d7db34c9e5495a052c70d0d03
jcrowe91 <jcrowe9119@gmail.com>
2022-03-06 19:32:09 -0600

    Built basic application CRUD functionality

--- PhoneBookApplication/Program.cs
+++ PhoneBookApplication/Program.cs
@@ -8,7 +8,7 @@ namespace PhoneBookConsoleUI
     {
         static void Main(string[] args)
         {
-            string userAnswer;
+            
             bool cont = true;
             var contacts = new List<Contact>();
             #region TEST CONTACT
@@ -21,49 +21,65 @@ namespace PhoneBookConsoleUI
             contacts.Add(test);
             #endregion
 
-
             Console.WriteLine("Welcome to your phonebook application!");
 
             while (cont == true)
             {
-
-
-                Console.WriteLine($"What would you like to do?\n1: View All Contacts\n2: Create a New Contact\n3: Update A Contact\n4: Delete A Contact\n5: Exit");
-                userAnswer = Console.ReadLine();
-
-                if (userAnswer == "1") //View All
-                {
-                    SeeAllContacts(contacts);
-
-
-                }
-                else if (userAnswer == "2") //Create
-                {
-                    Console.Clear();
-                    Console.WriteLine("Create New Contact:");
-                    var newContact = CreateContact();
-                    contacts.Add(newContact);
-
-                }
-                else if (userAnswer == "3") //Update
-                {
-                    UpdateContact(contacts);
-                }
-                else if (userAnswer == "4") //Delete
-                {
-                    Console.WriteLine("Four");
-                }
-                else if (userAnswer == "5")
-                {
-                    Console.WriteLine("Have a great day!");
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a valid response");
-
-                }
+                PhonebookMenu(contacts);               
             }
+        }
+
+        private static void PhonebookMenu(List<Contact> contacts)
+        {
+            string userAnswer;
+            Console.WriteLine($"What would you like to do?\n1: View All Contacts\n2: Create a New Contact\n3: Update A Contact\n4: Delete A Contact\n5: Exit");
+            userAnswer = Console.ReadLine();
+
+            if (userAnswer == "1") //View All
+            {
+                SeeAllContacts(contacts);
+            }
+            else if (userAnswer == "2") //Create
+            {
+                Console.Clear();
+                Console.WriteLine("Create New Contact:");
+                var newContact = CreateContact();
+                contacts.Add(newContact);
+
+            }
+            else if (userAnswer == "3") //Update
+            {
+                UpdateContact(contacts);
+            }
+            else if (userAnswer == "4") //Delete
+            {
+                DeleteContact(contacts);
+            }
+            else if (userAnswer == "5")
+            {
+                Console.WriteLine("Have a great day!");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid response");
+
+            }
+        }
+
+        private static void DeleteContact(List<Contact> contacts)
+        {
+            for (int i = 0; i < contacts.Count; i++)
+            {
+                Console.WriteLine($"ID: {i + 1} {contacts[i].FirstName} {contacts[i].LastName}\nPhone Number: {contacts[i].PhoneNumber}\nEmail Address: {contacts[i].EmailAddress}\nHome Address: {contacts[i].HomeAddress}");
+                Console.WriteLine("-----------------------------");
+            }
+
+            Console.WriteLine("What is the ID of the contact you'd like to delete?");
+            var contactID = Convert.ToInt32(Console.ReadLine());
+            
+            contacts.RemoveAt(contactID - 1);
+            Console.WriteLine("Success!");
         }
 
         private static void UpdateContact(List<Contact> contacts)
